f29b07f7b5c51bd59e58f692a5bce2351f561774
vikalp patil <vikalp174patil@gmail.com>
2019-03-14 19:07:11 +0530

    new one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Tab1Component } from './mod1/tab1.component';
+import { Tab2Component } from './mod1/tab2.component';
+import { Tab3Component } from './mod1/tab3.component';
+import { Tab4Component } from './mod1/tab4.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'tab1', component:  Tab1Component },
+  {path:'tab2', component:  Tab2Component },
+  {path:'tab3', component:  Tab3Component },
+  {path:'tab4', component:  Tab4Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents =[Tab1Component,Tab2Component,Tab3Component,Tab4Component]
